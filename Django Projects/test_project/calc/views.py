e2589b448ada70e525fd58bf1a7c87fc888a4c45
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2021-11-21 22:00:43 +0530

    Tutorial done till No.6!!

--- Django Projects/test_project/calc/views.py
+++ Django Projects/test_project/calc/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'home.html', {'name':'AAAA'})
