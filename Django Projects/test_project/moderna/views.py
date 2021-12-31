fa14f82672e3cef68e9773b865e2a40dc45fa82b
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2021-12-31 15:13:00 +0530

    Completed till tutorial #12!!!

--- Django Projects/test_project/moderna/views.py
+++ Django Projects/test_project/moderna/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'Moderna/index.html')
+    c1 = Card()
+    c2 = Card()
+    c3 = Card()
+    c1.title, c2.title, c3.title = 'Hey There!', 'This is A', 'Big Test'
+    c1.desc, c2.desc, c3.desc = 'This is D1', 'This is D2', 'This is D3'
+
+    return render(request, 'Moderna/index.html', {'c1':c1, 'c2':c2, 'c3':c3})
