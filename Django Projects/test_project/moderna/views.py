1b992cd5a4e19761d3d828fa8241c0022a52ec73
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2021-12-31 15:36:16 +0530

    Completed till tutorial #14!!! Lets goo!

--- Django Projects/test_project/moderna/views.py
+++ Django Projects/test_project/moderna/views.py
@@ -10,4 +10,5 @@ def index(request):
     c1.title, c2.title, c3.title = 'Hey There!', 'This is A', 'Big Test'
     c1.desc, c2.desc, c3.desc = 'This is D1', 'This is D2', 'This is D3'
 
-    return render(request, 'Moderna/index.html', {'c1':c1, 'c2':c2, 'c3':c3})
+    cards = [c1, c2, c3]
+    return render(request, 'Moderna/index.html', {'cards':cards})
