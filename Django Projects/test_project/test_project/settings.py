91bca5f3f9704da2232d3ca6fb53e9ee07ad7be0
School PC <omnamonarayanayashiva@gmail.com>
2022-01-10 09:37:57 -1200

    Working on #20

--- Django Projects/test_project/test_project/settings.py
+++ Django Projects/test_project/test_project/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
