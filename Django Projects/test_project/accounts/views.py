14d10a8411e72d138ee6a6a5584eb33653d52731
Arjun <omnamonarayanayashiva@gmail.com>
2022-01-14 21:54:26 +0530

    Completed Django Basic tutorial course!!!!!!!!!!!!!!! Lets goooo

--- Django Projects/test_project/accounts/views.py
+++ Django Projects/test_project/accounts/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['user']
+        password = request.POST['pass']
+        user = auth.authenticate(username=username, password=password)
+        if user != None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Wrong Username or Password')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
 
 def register(request):
 
@@ -15,16 +31,25 @@ def register(request):
 
         if password0 != password1:
             print('Password Mismatch')
+            messages.info(request, 'Password Mismatch')
+            return redirect('register')
         elif User.objects.filter(username=username).exists():
             print('Username Exists')
+            messages.info(request, 'Username Exists')
+            return redirect('register')
         elif User.objects.filter(email=email).exists():
             print('Email Exists')
+            messages.info(request, 'Email Exists')
+            return redirect('register')
         else:
             user = User.objects.create_user(username=username, first_name=fname, last_name=lname, email=email, password=password0)
             user.save()
             print('User Created')
-        
-        return redirect('/')
+            return redirect('login')
             
     else:
         return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
