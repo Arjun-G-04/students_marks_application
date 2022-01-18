3ff0b5dc4e6f00246460639290724a39eead8d16
Arjun <omnamonarayanayashiva@gmail.com>
2022-01-18 19:43:17 +0530

    Completed login page!!!

--- Django Projects/main_project/main_project/settings.py
+++ Django Projects/main_project/main_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'main_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ Django Projects/main_project/outside/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OutsideConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'outside'
